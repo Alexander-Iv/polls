409dbd560abde5d216efa88fd7f17a435d3adaac
ivanovaa <IvanovAA@tdc.ru.com>
2020-01-30 17:58:21 +1000

    Initial commit

---
+++ backend/init.sql
@@ -0,0 +1,6 @@
+CREATE USER polls PASSWORD 'yZnaaX4ZeIeVdahDedQQ';
+CREATE DATABASE polls OWNER polls;
+--GRANT ALL PRIVILEGES ON DATABASE postgres TO polls;
+--\connect polls;
+--SET ROLE polls;
+--CREATE SCHEMA polls;
